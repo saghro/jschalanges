3ce68643fc993473c423627924168531268b1532
saghro <ayoubsaghro27@gmail.com>
2023-09-21 11:25:33 +0100

    challenge10 jobintech

---
+++ main.js
@@ -0,0 +1,58 @@
+const personnes = [
+    ["yuba", "tinghir", 23],
+    ["hamza", "iknioune", 18],
+    ["hassan", "bomalne", 27],
+];
+
+function addPerson() {
+    const nom = prompt("Entrez le nom:");
+    const ville = prompt("Entrez la ville:");
+    const age = parseInt(prompt("Entrez l'âge:"));
+
+    if (nom && ville && !isNaN(age)) {
+        // Add the new person to the array
+        personnes.push([nom, ville, age]);
+        
+        // Clear the existing table
+        const tableauHTML = document.getElementById("table");
+        tableauHTML.innerHTML = "";
+
+        // Recreate and populate the table
+        const table = document.createElement("table");
+        
+        const thead = document.createElement("thead");
+        const headerRow = document.createElement("tr");
+
+        const headers = ["Prénom", "Ville", "Âge"];
+        headers.forEach((headerText) => {
+            const th = document.createElement("th");
+            th.textContent = headerText;
+            headerRow.appendChild(th);
+        });
+
+        thead.appendChild(headerRow);
+        table.appendChild(thead);
+
+        const tbody = document.createElement("tbody");
+
+        personnes.forEach((personne) => {
+            const row = document.createElement("tr");
+            personne.forEach((valeur) => {
+                const cell = document.createElement("td");
+                cell.textContent = valeur;
+
+                row.appendChild(cell);
+            });
+
+            tbody.appendChild(row);
+        });
+
+        table.appendChild(tbody);
+
+        tableauHTML.appendChild(table);
+    } else {
+        alert("Veuillez saisir des valeurs valides.");
+    }
+}
+
+document.getElementById("ajouterPersonne").addEventListener("click", addPerson);
